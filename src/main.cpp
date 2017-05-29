5b625628f9ccbfca6f2ddbe2cac3a4103edec2bc
Matthias Dieter Wallnöfer <mdw@samba.org>
2017-05-29 21:13:58 +0200

    remove outdated code

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
 #include "mainwidget.h"
 
-#include <QSharedMemory>
-
 int main( int argc, char ** argv ){
   qRegisterMetaType<sc_reader_t>("sc_reader_t");
   qRegisterMetaType<sc_card_t>("sc_card_t");
@@ -12,11 +10,6 @@ int main( int argc, char ** argv ){
   qRegisterMetaType<CardControlHandler::SerialData>("CardControlHandler::SerialData");
   qRegisterMetaType<X509CertificateHandler::X509CertificateData>("X509CertificateHandler::X509CertificateData");
   
-
-//   QSharedMemory shared("6d6f02f0-ed13-4132-b21e-be746ed4a623");
-//   if( !shared.create( 512, QSharedMemory::ReadWrite) )
-//     exit(0);
-
   QApplication a( argc, argv );
   bool hideMainWin = false;
   if ( argc == 2 && string(argv[1]) == "startHidden" )
